07380e58b160cca98bcfa19e7f440b531a5fa0fa
Antônio Carlos Melo Muniz Júnior <antonio.junior@inpartec.com.br>
2019-05-03 14:12:07 -0300

    BubbleSort

---
+++ BubbleSort/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Random rnd = new Random();
+
+            int[] numerosAleatorios = new int[100];
+
+            int temp = 0;
+
+            for (int i = 0; i < 100; i++)
+            {
+                numerosAleatorios[i] = rnd.Next(1, 100);
+            }
+
+            numerosAleatorios = NumerosAleatorios(numerosAleatorios);
+
+            for (int i = 0; i < numerosAleatorios.Length; i++)
+            {
+                Console.WriteLine(numerosAleatorios[i]);
+            }
+            Console.ReadKey();            
+        }
+
+        public static int[] NumerosAleatorios(int[] ret)
+        {
+
+            int temp = ret[0];
+
+            for (int i = 0; i < ret.Length; i++)
+            {
+                for (int j = i + 1; j < ret.Length; j++)
+                {
+                    if (ret[i] > ret[j])
+                    {
+                        temp = ret[i];
+
+                        ret[i] = ret[j];
+
+                        ret[j] = temp;
+                    }
+                }
+            }
+
+            return ret;
+        }
+    }
+}
+
---
+++ BubbleSort2/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BubbleSort2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            /*
+             * Deixei um codigo funcional de outra forma de fazer o bubbleSort. 
+             * OBS nao consegui melhorar a adicao na colecao
+             */
+            Random rnd = new Random();
+            List<numerosAleatorios> num = new List<numerosAleatorios>()
+            {
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+                new numerosAleatorios() { numeros = rnd.Next(0, 100) },
+
+            };
+
+
+            var ordNum = num.OrderBy(x => x.numeros);
+
+            foreach (var val in ordNum)
+            {
+                Console.WriteLine(val.numeros);
+            }
+
+            Console.ReadKey();
+
+    
+        }
+
+        class numerosAleatorios
+        {
+            public int numeros { get; set; }
+
+        }
+    }
+}
